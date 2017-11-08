8c15493459be4e81c3daa54e95cf87e910a4e8bb
Patrick <patzila@gmail.com>
2017-11-08 18:12:43 -0500

    Finished the stubs

--- lib/Board.rb
+++ lib/Board.rb
@@ -2,7 +2,7 @@
 # Class: Board_View
 # Date Created: Nov. 2 2017
 #
-# Descirption:  The game baord and it's functions.
+# Description:  The game baord and it's functions.
 ####################################################################
 
 class Board
@@ -11,7 +11,7 @@ class Board
     ####################################################################
     # Method: initialize
     #
-    # Descirption: - Initializes the board
+    # Description: - Initializes the board
     #
     ####################################################################
     def initialize()
@@ -28,7 +28,7 @@ class Board
     ####################################################################
     # Method: action
     #
-    # Descirption: Checks if the attempted move is valid and if so
+    # Description: Checks if the attempted move is valid and if so
     #              ensures the move gets made.
     #
     # Arguments:   new_position    : A position that a piece is moving from. (int array [xPos,yPos]) 
@@ -46,7 +46,7 @@ class Board
     ####################################################################
     # Method: count_opponent
     #
-    # Descirption: Check if there are any pieces of colour opponent_colour
+    # Description: Check if there are any pieces of colour opponent_colour
     #              on the board.
     #
     # Arguments: - opponent_colour: A symbol of either :symbol_1 or :symbol_2
@@ -61,7 +61,7 @@ class Board
     ####################################################################
     # Method: validate
     #
-    # Descirption: Determines if a move from the inital_postion to 
+    # Description: Determines if a move from the iniital_postion to 
     #              the new_positon is valid.
     # 
     #
@@ -81,9 +81,23 @@ class Board
     end
 
     ####################################################################
+    # Method: validate_piece    
+    #
+    # Description: Returns if the position selected holds a valid piece
+    # 
+    #
+    # Arguments:   initial_position    : The position to validate     (int array [xPos,yPos]) 
+    #
+    # Returns:     True :  If the position selected holds a valid piece
+    #              False:  Otherwise
+    ####################################################################
+    def validate_piece(initial_position)
+    end
+
+    ####################################################################
     # Method: validate_neighbors
     #
-    # Descirption: Checks if the selected position is valid.
+    # Description: Checks if the selected position is valid.
     #
     # Arguments:   new_position    : Position a piece is moving to.     (int array [xPos,yPos]) 
     #              initial_position: Position a piece is moving from to (int array [xPos,yPos]) 
@@ -92,23 +106,129 @@ class Board
     #          False: Otherwise
     ####################################################################
     def validate_neighbors(new_position, initial_position)
-
+        
         # var = get_neighbours()
         # if newPosition is in var, move is valid
     end
 
+    ####################################################################
+    # Method: get_neighbors
+    #
+    # Description: Based on strength  returns a list of all possible locations to move
+    #
+    # Arguments: initial_position: Position a piece 
+    #
+    # Returns: List[][2] the list of moves that are valid
+    ####################################################################
+    def get_neighbors(initial_position)
 
+    end
+
+    ####################################################################
+    # Method: validate_direction
+    #
+    # Description: Returns if a piece can move in that direction since a piece cannot move in the same direction twice
+    #
+    # Arguments: new_position: New Position a piece 
+    # Arguments: initial_position: Old Position a piece 
+    #
+    # Returns: If it was a valid direction
+    ####################################################################
+    def validate_direction(new_position, initial_position)
+        #calls find_direction()
+    end
+
+    ####################################################################
+    # Method: find_direction
+    #
+    # Description: Determines a direction from the given positions
+    #
+    # Arguments: new_position: New Position a piece 
+    # Arguments: initial_position: Old Position a piece 
+    #
+    # Returns: Symbol() either N,S,E,W,NE,SE,SW,NW
+    ####################################################################
+    def find_direction(new_position, initial_postion)
+
+    end
+
+    ####################################################################
+    # Method: move_type
+    #
+    # Description: returns a symbol associate with the move type of the move the player made
+    #
+    # Arguments: new_position: New Position a piece 
+    # Arguments: initial_position: Old Position a piece 
+    #
+    # Returns: Symbol A,W,P,N
+    ####################################################################
+    def move_type(new_position, initial_position)
+        
+    end
+
+    ####################################################################
+    # Method: is_approach
+    #
+    # Description: determines is a move is a withdrawf
+    #
+    # Arguments: new_position: New Position a piece 
+    # spec only has one param
+    #
+    # Returns: Symbol A,W,P,N
+    ####################################################################
+    def is_approach(new_position)
+        
+    end
+    ####################################################################
+    # Method: is_withdraw
+    #
+    # Description: determines if a move is a withdraw
+    #
+    # Arguments: new_position: New Position a piece 
+    # Note spec only has one param
+    #
+    # Returns: Bool
+    ####################################################################
+    def is_withdraw(new_position)
+        
+    end
+                        
     ####################################################################
     # Method: capture_avaliable
     #
-    # Descirption: Looks at the board_hash and checks if there are any approach moves available
+    # Description: Looks at the board_hash and checks if there are any approach moves available
     #
     #
     # Returns:     True :  If an approach move is available
     #              False:  Otherwise
     ####################################################################
     def capture_avaliable()
+        
+    end
+    ####################################################################
+    # Method: update_board
+    #
+    # Description: Updates the board with the affected pieces given
+    #
+    # Arguments: affected_pieces: a list of affected pieces List[][3]
+    #
+    # Returns:     Void
+    ####################################################################
+    def update_board(affected_pieces)
+        
+    end
 
+    ####################################################################
+    # Method: set_last_direction
+    #
+    # Description: Updates the last direction if it changed
+    #
+    #
+    # Returns:     True :  If the direction changed
+    #              False:  Otherwise
+    ####################################################################
+    def set_last_direction()
+        
     end
 
 
