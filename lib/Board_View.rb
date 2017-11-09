ed87a11600f2e91ebd75479bba498d494cbe9ca7
Nikolas Orkic <niorkic@gmail.com>
2017-11-09 10:06:37 -0500

    Adding some board print mechanism that may or may not be working

--- lib/Board_View.rb
+++ lib/Board_View.rb
@@ -27,5 +27,39 @@ class Board_View
     def update()
         # Loops through each corrdinate of the board and asks it what
         # piece colour it has on it. print the piece colour to the screen.
+
+        conenction = "\\"
+        # Board size is 9x5
+        for i in 8..0 do
+            # If i is even, print number, and the pieces on the board.
+            # Else, print a row of conenecting lines
+            if (i%2 == 0)
+                puts ("#{i/2}")
+                # Print the pieces on the board
+                for j in 0..8 do
+                    puts "-"
+                    piece = current_board.board_hash("#{i,j}") 
+                    if (piece == :black)
+                        puts "x"
+                    elsif (piece == :white)
+                        puts "o"
+                    else
+                        puts "+"
+                    end
+                end
+
+            else
+                for j in 0..8 do
+                    puts("|#{conenction}")
+                    if (j != 8)
+                        if (conenction == "\\")
+                            conenction == "/"
+                        else
+                            conenction == "\\"
+                        end
+                    end
+                end
+            end
+        end
     end
 end
