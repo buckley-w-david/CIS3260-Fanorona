1ff826c1f94befa47634c3c89eeeb8108018b661
Nikolas Orkic <norkic@mail.uoguelph.ca>
2017-11-09 22:49:28 -0500

    fixing typos....

--- bin/fanorona.rb
+++ bin/fanorona.rb
@@ -2,7 +2,7 @@
 # Class: fanarona.rb
 # Date Created: Nov. 2 2017
 #
-# Descirption:  Entry point for the game Fanorona. Initialize classes
+# Description:  Entry point for the game Fanorona. Initializes classes
 #               in the Lib folder and runs the game.
 ####################################################################
 
