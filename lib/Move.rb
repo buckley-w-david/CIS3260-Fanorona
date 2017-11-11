45c08a31e0d6ecffb938ebb71af514bad0e27edc
Jordon Smith <jsmith72@mail.uoguelph.ca>
2017-11-11 18:30:55 -0500

    Implemented moves

--- lib/Move.rb
+++ lib/Move.rb
@@ -7,11 +7,11 @@
 #              that the move will have, The Intersections class uses
 #              them to actually update the board.
 ####################################################################
+# require_relative 'Board'
 
 class Move
 
 
-load 'Board.rb'
     ####################################################################
     # Method: initialize
     #
@@ -44,8 +44,16 @@ load 'Board.rb'
     ####################################################################
     def paika(initial, next_position)
 
-            initial_colour = @board.board_hash[array_to_board_key(initial)]
-            puts(initial_colour)
+        # append the initial position as empty
+        affected_pieces = Array.new
+        affected_pieces << [initial[0], initial[1], :E]
+
+        # retreive the colour of the piece being moved
+        initial_key = array_to_board_key(initial)
+        initial_colour = colour_to_symbol(@board_hash[initial_key])
+
+        # append the new position as the colour of the piece that was moved
+        affected_pieces << [next_position[0], next_position[1], initial_colour]
     end
 
     ####################################################################
@@ -68,6 +76,59 @@ load 'Board.rb'
     ####################################################################
     def withdrawl(initial, next_position)
 
+        # append the initial position as empty
+        affected_pieces = self.paika(initial, next_position)
+        # retreive the colour of the piece being moved
+        initial_key = array_to_board_key(initial)
+        initial_colour = colour_to_symbol(@board_hash[initial_key])
+
+        ### Taken from Board.find_direction ###
+        # need to find direction of movement as a positive, negative, or zero
+        # in order to calculate path of movement
+        pos_diff = [
+            next_position[0].to_i - initial[0].to_i,
+            next_position[1].to_i - initial[1].to_i
+        ]
+
+        # Need to scale the value down to eliminate extra cases to change. e.g. moving from position 2,2 to 0,0
+        move_transformation = []
+        move_transformation[0] = pos_diff[0] == 0 ? 0 : pos_diff[0]/pos_diff[0].abs
+        move_transformation[1] = pos_diff[1] == 0 ? 0 : pos_diff[1]/pos_diff[1].abs
+
+        # invert the move transformation in order to evaluate the opposite direction
+        withdrawl_move_transformation = []
+        withdrawl_move_transformation[0] = move_transformation[0] * -1
+        withdrawl_move_transformation[1] = move_transformation[1] * -1
+
+        # apply the transformation as a move
+        interim_position = []
+        interim_position[0] = initial[0] + withdrawl_move_transformation[0]
+        interim_position[1] = initial[1] + withdrawl_move_transformation[1]
+
+        # repeat transformation until an empty space or initial_colour is found
+        while (@board_hash[array_to_board_key(interim_position)])
+
+            # retrieve position from hash
+            transformed_state = colour_to_symbol(@board_hash[array_to_board_key(interim_position)])
+            # puts(array_to_board_key(interim_position) + "#{transformed_state}")
+
+            # check transformed positon state
+            if transformed_state != initial_colour
+
+                # append position if affected
+                affected_piece = [interim_position[0], interim_position[1], transformed_state]
+                affected_pieces << affected_piece
+            else
+                # position is not affected and the move can exit
+                return affected_pieces
+            end
+
+            # continue to apply transformation
+            interim_position[0] += withdrawl_move_transformation[0]
+            interim_position[1] += withdrawl_move_transformation[1]
+        end
+
+        return affected_pieces
     end
 
 
@@ -91,6 +152,55 @@ load 'Board.rb'
     ####################################################################
     def approach(initial, next_position)
 
+                # append the initial position as empty
+                affected_pieces = self.paika(initial, next_position)
+                # retreive the colour of the piece being moved
+                initial_key = array_to_board_key(initial)
+                initial_colour = colour_to_symbol(@board_hash[initial_key])
+
+                ### Taken from Board.find_direction ###
+                # need to find direction of movement as a positive, negative, or zero
+                # in order to calculate path of movement
+                pos_diff = [
+                    next_position[0].to_i - initial[0].to_i,
+                    next_position[1].to_i - initial[1].to_i
+                ]
+
+                # Need to scale the value down to eliminate extra cases to change. e.g. moving from position 2,2 to 0,0
+                move_transformation = []
+                move_transformation[0] = pos_diff[0] == 0 ? 0 : pos_diff[0]/pos_diff[0].abs
+                move_transformation[1] = pos_diff[1] == 0 ? 0 : pos_diff[1]/pos_diff[1].abs
+
+
+                # apply the transformation as a move
+                interim_position = []
+                interim_position[0] = next_position[0] + move_transformation[0]
+                interim_position[1] = next_position[1] + move_transformation[1]
+
+                # repeat transformation until an empty space or initial_colour is found
+                while (@board_hash[array_to_board_key(interim_position)])
+
+                    # retrieve position from hash
+                    transformed_state = colour_to_symbol(@board_hash[array_to_board_key(interim_position)])
+                    # puts(array_to_board_key(interim_position) + "#{transformed_state}")
+
+                    # check transformed positon state
+                    if transformed_state != initial_colour
+
+                        # append position if affected
+                        affected_piece = [interim_position[0], interim_position[1], transformed_state]
+                        affected_pieces << affected_piece
+                    else
+                        # position is not affected and the move can exit
+                        return affected_pieces
+                    end
+
+                    # continue to apply transformation
+                    interim_position[0] += move_transformation[0]
+                    interim_position[1] += move_transformation[1]
+                end
+
+                return affected_pieces
     end
 
 
@@ -100,7 +210,15 @@ def array_to_board_key(array)
     board_key = "#{array[0]},#{array[1]}"
 end
 
-newBoard = Board.new()
-newMove = Move.new(newBoard)
-newMove.paika([0,1], [0,2])
-puts(array_to_board_key([0,1]))
+def colour_to_symbol(colour)
+    case colour
+    when :Black
+        return :B
+    when :White
+        return :W
+    when :Empty
+        return :E
+    end
+
+    return nil
+end
